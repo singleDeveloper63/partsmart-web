aebc1eda0302d9dfdc11b1a7563ab58eead142e5
singledeveloper63 <singledeveloper63@gmail.com>
2021-01-31 16:29:40 +0500

    adfasdfasdf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.scss';
 import { Header } from './components';
+import { useEffect } from 'react';
 import { Home , ProductActions} from './pages'
 import { Switch , Route } from 'react-router-dom';
 import 'popper.js';
 import 'bootstrap/dist/js/bootstrap';
 import 'react-alice-carousel/lib/alice-carousel.css'
+import { useSelector , useDispatch } from 'react-redux';
+import { UIActions } from './redux/actions';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const { brands } = useSelector(({ ui }) => (ui))
+
+  useEffect(()=>{
+      dispatch(UIActions.getBrands());
+  },[])
+
+  useEffect(()=>{
+      brands.list.forEach( async (brand) => {
+          const res = await dispatch(UIActions.getCarModels(brand.id));
+          dispatch(UIActions.setBrandCars({ id : brand.id , cars : res.payload }))
+      })
+  },[brands])
 
   document.onselectstart = function(){ return false; }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,21 +7,12 @@ import { UIActions } from '../../redux/actions';
 function Home(){
 
     const dispatch = useDispatch();
-    const { sliders , brands , products } = useSelector(({ ui }) => (ui))
+    const { sliders , products } = useSelector(({ ui }) => (ui))
 
     useEffect(()=>{
-        dispatch(UIActions.getBrands());
         dispatch(UIActions.getSlides());
         dispatch(UIActions.getProducts());
     },[])
-
-    useEffect(()=>{
-        brands.list.forEach( async (brand) => {
-            const res = await dispatch(UIActions.getCarModels(brand.id));
-            dispatch(UIActions.setBrandCars({ id : brand.id , cars : res.payload }))
-        })
-    },[brands])
-
 
     return(
         <div className="home">
