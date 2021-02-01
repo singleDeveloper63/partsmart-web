13c746897d04ea7a9b2175f8e93147d31810bf93
singledeveloper63 <singledeveloper63@gmail.com>
2021-02-02 00:04:21 +0500

    sdfG

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import rootReducer from './redux/reducers';
 
 
 const store = createStore(rootReducer , compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
 ));
 
 ReactDOM.render(
